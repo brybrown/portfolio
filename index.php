ddc882817bea4139e957bd1e7444560645d527d6
Bryson Brown <26bbrown5@go.dsdmail.net>
2023-09-30 22:59:09 -0600

    minor changes and WE'RE NOW USING PHP!!

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php 
+		require "helpful-html/head.html";
+	?>
+
+	<body>
+		<?php
+			require "helpful-html/headerbar.html";
+		?>
+
+		<main>
+			<div class="cardsdiv">
+				<div class="card">
+					<a href="portfolio.php">
+						<img src="assets/images/prints.png" alt="paw prints">
+						
+						<h3>Skill</h3>
+					</a>
+				</div>
+			</div>
+		</main>
+	</body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<?php 
+		require "helpful-html/head.html";
+	?>
+
+    <body>
+        <header>
+			<div class="logo">
+				<a href="index.php">
+					<img src="assets/images/logo/circle.png" alt="Logo">
+				</a>
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<a href="portfolio.html">
+							Portfolio
+						</a>
+					</li>
+
+					<li>
+						<a href="contact.html">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+        
+    </body>
+</html>
