4f26df8c352837c205c8ddce2c7e0534f9881ebb
Bryson Brown <26bbrown5@go.dsdmail.net>
2023-10-04 11:09:13 -0600

    Started on portfolio and added My Bike Sold site files.

--- portfolio.php
+++ portfolio.php
@@ -9,6 +9,14 @@
 			require "helpful-html/headerbar.html";
 		?>
 
-		
+		<div class="cardsdiv">
+			<div class="card">
+				<a href="portfolio.php">
+					<img src="assets/images/my-bike-sold.png" alt="my bike sold site">
+						
+					<h3>My&nbsp;Bike&nbsp;Sold</h3>
+				</a>
+			</div>
+		</div>
 	</body>
 </html>
